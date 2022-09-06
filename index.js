7f5528d9244ac6446f30e9d846882f66f8b1b2be
marianelamunozgutierrez@gmail.com <marianelamunozgutierrez@gmail.com>
2022-09-06 16:51:22 -0400

    morgan middlware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
