e445062b669701302a4bc324eb9ac1adc0547bcb
marianelamunozgutierrez@gmail.com <marianelamunozgutierrez@gmail.com>
2022-09-12 16:07:57 -0400

    server error managment

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const mongoose =require("mongoose");
+process.on("uncaughtException", (err) => {
+    console.log("uncaughtException");
+    console.log("Shutting dow");
+    process.exit(1);
+});
 const Product = require("./models/Product");
 const app = require("./app");
 const port = process.env.PORT;
@@ -6,7 +11,14 @@ mongoose.connect(process.env.DATABASE, {}).then((con) => {
     console.log("Connected to mongo");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
 
+process.on("unhandledRejection", (err) => {
+    console.log("unhandledRejection");
+    console.log("Shutting dow");
+    server.close(() => {
+        process.exit(1);
+    });
+});
